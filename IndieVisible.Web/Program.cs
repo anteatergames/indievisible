c59abde00d61452e3fc0098b7a8fb9b6a25f5f53
programad <programad@gmail.com>
2019-07-19 14:26:15 -0300

    UseIISIntegration

--- IndieVisible.Web/Program.cs
+++ IndieVisible.Web/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace IndieVisible.Web
 {
@@ -19,6 +12,7 @@ namespace IndieVisible.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
