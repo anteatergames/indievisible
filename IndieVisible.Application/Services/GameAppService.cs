03035f7f421cddbfb17a888a53ba6d095c057a56
Daniel Gomes <programad@gmail.com>
2020-01-03 14:07:06 -0300

    Game thumbnails w_278

--- IndieVisible.Application/Services/GameAppService.cs
+++ IndieVisible.Application/Services/GameAppService.cs
@@ -180,7 +180,7 @@ namespace IndieVisible.Application.Services
 
             foreach (GameListItemViewModel item in vms)
             {
-                item.ThumbnailUrl = string.IsNullOrWhiteSpace(item.ThumbnailUrl) || Constants.DefaultGameThumbnail.Contains(item.ThumbnailUrl) ? Constants.DefaultGameThumbnail : UrlFormatter.Image(item.UserId, BlobType.GameThumbnail, item.ThumbnailUrl);
+                item.ThumbnailUrl = string.IsNullOrWhiteSpace(item.ThumbnailUrl) || Constants.DefaultGameThumbnail.Contains(item.ThumbnailUrl) ? Constants.DefaultGameThumbnail : UrlFormatter.Image(item.UserId, BlobType.GameThumbnail, item.ThumbnailUrl, 278);
                 item.DeveloperImageUrl = UrlFormatter.ProfileImage(item.UserId);
 
                 UserProfile authorProfile = GetCachedProfileByUserId(item.UserId);
