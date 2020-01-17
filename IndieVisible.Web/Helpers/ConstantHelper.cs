112ff4503aba975ade06e2f49e7c1b970746a720
Daniel Gomes <programad@gmail.com>
2020-01-17 10:52:14 -0300

    SEO - Titles shorten

--- IndieVisible.Web/Helpers/ConstantHelper.cs
+++ IndieVisible.Web/Helpers/ConstantHelper.cs
@@ -2,7 +2,7 @@
 {
     public static class ConstantHelper
     {
-        public static string DefaultTitle { get { return "INDIEVISIBLE - The Independent Game Developers Social Network"; } }
+        public static string DefaultTitle { get { return "INDIEVISIBLE - The indie gamedev social network"; } }
 
         public static string DefaultDescription { get { return "The independent game developers social network. Nothing can divide us!"; } }
     }
