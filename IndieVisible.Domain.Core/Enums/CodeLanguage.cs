480402af1aeb7a7a031a578900d65cfff41d3576
Daniel Gomes <programad@gmail.com>
2020-02-21 14:36:56 -0300

    Game details fixes

--- IndieVisible.Domain.Core/Enums/CodeLanguage.cs
+++ IndieVisible.Domain.Core/Enums/CodeLanguage.cs
@@ -1,11 +1,18 @@
-﻿namespace IndieVisible.Domain.Core.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IndieVisible.Domain.Core.Enums
 {
     public enum CodeLanguage
     {
+        [Display(Name= "C#")]
         Csharp,
+        [Display(Name = "Java")]
         Java,
+        [Display(Name = "C++")]
         Cplusplus,
+        [Display(Name = "Javascript")]
         Javascript,
+        [Display(Name = "GML")]
         Gml
     }
 }
