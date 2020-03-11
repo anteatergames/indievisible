136ac7f3645e84b074a3c7d882b8df670e8bed48
Daniel Gomes <programad@gmail.com>
2020-03-11 00:42:22 -0300

    decimal percentage

--- IndieVisible.Application/Services/TranslationAppService.cs
+++ IndieVisible.Application/Services/TranslationAppService.cs
@@ -425,7 +425,7 @@ namespace IndieVisible.Application.Services
         {
             int totalTranslationsTarget = languageCount * totalTerms;
 
-            int percentage = (100 * translatedCount) / (totalTranslationsTarget == 0 ? 1 : totalTranslationsTarget);
+            double percentage = (100 * translatedCount) / (double)(totalTranslationsTarget == 0 ? 1 : totalTranslationsTarget);
 
             return percentage;
         }
