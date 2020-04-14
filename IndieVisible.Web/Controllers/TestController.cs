c736b2b3d7542390c203d13360f03da0211754cb
Daniel Gomes <programad@gmail.com>
2020-04-14 10:04:07 -0300

    Discord icon on infobox

---
+++ IndieVisible.Web/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using IndieVisible.Web.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IndieVisible.Web.Controllers
+{
+    public class TestController : SecureBaseController
+    {
+        public IActionResult DiscordWidget()
+        {
+            return View();
+        }
+    }
+}
