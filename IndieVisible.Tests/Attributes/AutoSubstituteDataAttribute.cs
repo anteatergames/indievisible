58e31137d24db26edbe2b578feb5f0ffe22ce170
Daniel Gomes <programad@gmail.com>
2020-05-08 22:34:01 -0300

    Tests are back!

---
+++ IndieVisible.Tests/Attributes/AutoSubstituteDataAttribute.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndieVisible.Tests.Attributes
+{
+    internal class AutoSubstituteDataAttribute : AutoDataAttribute
+    {
+        public AutoSubstituteDataAttribute() : base(() =>
+            {
+                var fixture = new Fixture().Customize(new AutoNSubstituteCustomization { ConfigureMembers = true });
+
+                fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList().ForEach(b => fixture.Behaviors.Remove(b));
+
+                fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+                return fixture;
+            })
+        {
+        }
+    }
+}
---
+++ IndieVisible.Tests/Domain/GameDomainServiceTests.cs
@@ -0,0 +1,67 @@
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using FluentAssertions;
+using IndieVisible.Domain.Interfaces.Repository;
+using IndieVisible.Domain.Interfaces.Service;
+using IndieVisible.Domain.Models;
+using IndieVisible.Domain.Services;
+using IndieVisible.Tests.Attributes;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IndieVisible.Tests.Domain
+{
+    [Trait("Category", "Domain")]
+    public class GameDomainServiceTests
+    {
+        public IFixture Fixture { get; }
+
+        public GameDomainServiceTests()
+        {
+            var customization = new AutoNSubstituteCustomization { ConfigureMembers = true };
+
+            Fixture = new Fixture().Customize(customization);
+            Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+
+        [Theory]
+        [Trait("Method", "GetAll")]
+        [AutoSubstituteData]
+        public async Task ShouldReturnGames(IGameRepository repository)
+        {
+            var sut = new GameDomainService(repository);
+
+            var result = sut.GetAll();
+
+            await repository.Received().GetAll();
+
+
+            result.Should().NotBeNull();
+            result.Should().HaveCountGreaterOrEqualTo(0);
+        }
+
+        [Theory]
+        [Trait("Method", "GetAll")]
+        [AutoSubstituteData]
+        public async Task ShouldReturnSingleGame(IGameRepository repository)
+        {
+            var obj = Fixture.Create<Game>();
+
+            repository.GetById(obj.Id).Returns(obj);
+
+            var sut = new GameDomainService(repository);
+
+            var result = sut.GetById(obj.Id);
+
+            await repository.Received().GetById(obj.Id);
+
+
+            result.Should().NotBeNull();
+
+            result.Id.Should().Be(obj.Id);
+        }
+    }
+}
